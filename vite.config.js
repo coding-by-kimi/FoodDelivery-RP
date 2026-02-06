bcc1c398170478a2b123ff28bb9bf8ae83a997dc
Kimti <sunnychauhan2413@gmail.com>
2026-02-06 17:54:55 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base : '/Delivery-BurgerRP'
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FoodDelivery-RP/'
+  base: '/FoodDelivery-RP/',
 })
